380d9e52ecd729dba460b4cbdd05f20bc834e909
Felipe Dos Santos <felipe.santosmf2022@gmail.com>
2023-10-08 01:36:40 -0300

    Classe de Serviço para gerar LumBot

---
+++ src/main/java/org/example/application/servicebot/GenerateBotService.java
@@ -0,0 +1,42 @@
+package org.example.application.servicebot;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import org.example.application.LumBot;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateBotService {
+    public static JDABuilder provideLum(){
+        JDABuilder lum = null;
+        try{
+            LumBot lumBot = new LumBot(cacheFlags(),gatewayIntents(),listenerAdapters());
+            lum = lumBot.builder();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        return lum;
+
+    }
+
+    private static List<GatewayIntent> gatewayIntents(){
+        List<GatewayIntent> list = Arrays.asList(GatewayIntent.MESSAGE_CONTENT,GatewayIntent.DIRECT_MESSAGES,GatewayIntent.GUILD_PRESENCES);
+        return list;
+    }
+
+    private static List<CacheFlag> cacheFlags(){
+        List<CacheFlag> list = Arrays.asList(CacheFlag.ACTIVITY);
+        return list;
+    }
+
+    private static List<ListenerAdapter> listenerAdapters(){
+        List<ListenerAdapter> list = Arrays.asList();
+        return list;
+    }
+}
