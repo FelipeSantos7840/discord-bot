e5d74a34dc178084f2e4935e5848fe39b27b23da
Felipe Dos Santos <felipe.santosmf2022@gmail.com>
2023-10-08 00:24:52 -0300

    Implementação Inicial da Classe LumBot

--- src/main/java/org/example/application/LumBot.java
+++ src/main/java/org/example/application/LumBot.java
@@ -1,7 +1,42 @@
 package org.example.application;
 
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LumBot {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private String token;
+    private List<CacheFlag> cacheFlags;
+    private List<GatewayIntent> gatewayIntents;
+    private List<ListenerAdapter> listerners = new ArrayList<>();
+
+    public LumBot() throws IOException {
+        String path = ("data/botToken.txt");
+        BufferedReader bfr = new BufferedReader(new FileReader(path));
+        String line = bfr.readLine();
+        if(line == null){
+            throw new IOException("Bot sem Token informado!");
+        }
+        this.token = line;
+        cacheFlags = Arrays.asList(CacheFlag.ACTIVITY,CacheFlag.ROLE_TAGS);
+        gatewayIntents = Arrays.asList(GatewayIntent.MESSAGE_CONTENT,GatewayIntent.GUILD_MEMBERS);
     }
+
+    public String getToken() {
+        return token;
+    }
+
+    public List<ListenerAdapter> getListerners(){
+        return listerners;
+    }
+
 }
